c4a15d9f8cb8112c3c1a6dd604e41f6e8b153a60
bekrichkid <isroilbeknuriddinov4@gmail.com>
2025-08-21 15:53:33 +0500

    asdasdasfasdasdsdsad

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
